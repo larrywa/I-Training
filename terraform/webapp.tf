f354c77e3440a17acfae0026bacad80fd5007aa2
Larry Wall <larrywa@users.noreply.github.com>
2024-05-14 19:44:47 -0400

    Update webapp.tf

--- terraform/webapp.tf
+++ terraform/webapp.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name                = "tfwebapp-asp-${random_integer.ri.result}"
+  name                = "webapp-asp-${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -35,7 +35,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                  = "tfwebapp-${random_integer.ri.result}"
+  name                  = "webapp-${random_integer.ri.result}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
