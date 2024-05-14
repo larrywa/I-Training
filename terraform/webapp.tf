91c95fec1c490efa0d4864d194b33a84e0786224
Larry Wall <larrywa@users.noreply.github.com>
2024-05-14 19:31:42 -0400

    Update webapp.tf

--- terraform/webapp.tf
+++ terraform/webapp.tf
@@ -19,14 +19,14 @@ resource "random_integer" "ri" {
 }
 
 # Create the resource group
-resource "azurerm_resource_group" "rgLinuxTF" {
-  name     = "myResourceGroup-${random_integer.ri.result}"
+resource "azurerm_resource_group" "rg" {
+  name     = "rglinuxtf-${random_integer.ri.result}"
   location = "eastus"
 }
 
 # Create the Linux App Service Plan
-resource "azurerm_service_plan" "tfappserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "tfwebapp-asp-${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
@@ -34,8 +34,8 @@ resource "azurerm_service_plan" "tfappserviceplan" {
 }
 
 # Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "tfwebapp" {
-  name                  = "webapp-${random_integer.ri.result}"
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "tfwebapp-${random_integer.ri.result}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
