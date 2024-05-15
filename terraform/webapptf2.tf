774aa7fdbd1fbb2bceced531c856d70088023adf
Larry Wall <larrywa@users.noreply.github.com>
2024-05-15 12:42:53 -0400

    Update webapptf2.tf

--- terraform/webapptf2.tf
+++ terraform/webapptf2.tf
@@ -26,7 +26,7 @@ data "azurerm_resource_group" "rg" {
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "webapp-asp-${random_integer.ri.result}"
-  location            = var.location
+  location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "B1"
@@ -35,7 +35,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
   name                  = "webapp-${random_integer.ri.result}"
-  location              = var.location
+  location              = data.azurerm_resource_group.rg.location
   resource_group_name   = data.azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
   https_only            = true
